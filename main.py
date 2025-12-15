6efb22c6fdd78b17a80e1ef37b91310e58b11026
SDLX <1654255+starmandeluxe@users.noreply.github.com>
2025-12-15 11:59:23 +0900

    Implement iterative Fibonacci function in main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+
+def _find_fib_iterative(x: int) -> int:
+    # 0, 1, 1, 2, 3, 5, 8, 13...
+
+    base = 1
+    prev = 0
+
+    if x == 1:
+        return 0
+    if x == 2:
+        return 1
+
+    for i in range(2, x):
+        next_num = base + prev
+        prev = base
+        base = next_num
+    return next_num
+
+
+if __name__ == '__main__':
+  fib_index = int(sys.argv[1])
+  print(_find_fib_iterative(fib_index))
